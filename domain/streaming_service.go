1419f69ff3d05588c32dfba482d1519071140976
Romain Libaert <git@rlibaert.dev>
2026-06-06 21:36:42 +0200

    feat: buffer & burst data

--- domain/streaming_service.go
+++ domain/streaming_service.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"io"
 	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/rlibaert/gocast/domain/internal"
@@ -70,7 +71,7 @@ func (svc *streamingService) Publish(ctx context.Context, s StreamPub, r io.Read
 		}
 	}()
 
-	return io.Copy(internal.FuncWriter(func(p []byte) (int, error) {
+	w := internal.FuncWriter(func(p []byte) (int, error) {
 		svc.mu.RLock()
 		defer svc.mu.RUnlock()
 
@@ -81,14 +82,16 @@ func (svc *streamingService) Publish(ctx context.Context, s StreamPub, r io.Read
 
 			ps, _ := svc.streamsPubsub.Load(sub)
 			if ps == nil {
-				ps, _ = svc.streamsPubsub.LoadOrStore(sub, internal.NewPubsub())
+				ps, _ = svc.streamsPubsub.LoadOrStore(sub, newStreamingPubsub())
 			}
 
 			ps.(internal.Pubsub).Write(p) //nolint: errcheck,gosec // always valid and never fails
 		}
 
 		return len(p), nil
-	}), r)
+	})
+
+	return io.Copy(w, r)
 }
 
 func (svc *streamingService) Subscribe(ctx context.Context, s StreamSub, w io.Writer) (int64, error) {
@@ -102,6 +105,48 @@ func (svc *streamingService) Subscribe(ctx context.Context, s StreamSub, w io.Wr
 	default:
 		return ps.(internal.Pubsub).WriteTo(w) //nolint: errcheck // always valid
 	}
+}
+
+// streamingPubsub wraps an [internal.Pubsub] to buffer data and burst some when a new subscriber connects.
+type streamingPubsub struct {
+	internal.Pubsub
+
+	bufferingSince time.Time
+	buffer         *[]byte
+	burst          atomic.Pointer[[]byte]
+}
+
+func newStreamingPubsub() internal.Pubsub {
+	ps := streamingPubsub{
+		Pubsub:         internal.NewPubsub(),
+		bufferingSince: time.Now(),
+		buffer:         new([]byte),
+	}
+	ps.burst.Store(new([]byte))
+	return &ps
+}
+
+func (ps *streamingPubsub) Write(p []byte) (int, error) {
+	*ps.buffer = append(*ps.buffer, p...)
+	if time.Since(ps.bufferingSince) < 3*time.Second {
+		return len(p), nil
+	}
+
+	_, err := ps.Pubsub.Write(*ps.buffer)
+	ps.bufferingSince = time.Now()
+	ps.buffer = ps.burst.Swap(ps.buffer)
+	*ps.buffer = (*ps.buffer)[:0]
+
+	return len(p), err
+}
+
+func (ps *streamingPubsub) WriteTo(w io.Writer) (int64, error) {
+	n, err := w.Write(*ps.burst.Load())
+	if err != nil {
+		return int64(n), err
+	}
+	m, err := ps.Pubsub.WriteTo(w)
+	return int64(n) + m, err
 }
 
 type debouncedStreamingService struct {
